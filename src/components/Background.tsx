a24547e3b1c6275dda49f0ad5efe9ab04fd47faf
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-17 13:39:19 +0545

    style: Revert code to red background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -28,7 +28,7 @@ const Background = () => {
   }, []);
 
   return (
-    <div className="bg-blue-900 absolute top-0 left-0 bottom-0 right-0 -z-10">
+    <div className="bg-red-900 absolute top-0 left-0 bottom-0 right-0 -z-10">
       <div className="relative">
         {particles.map((value) => (
           <Particle
