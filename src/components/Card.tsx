3306a73edb97f9397b46421b4f1ab4b793ac9793
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-21 23:47:08 +0545

    refactor: destructure the card state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 
 const Card = ({ cardStage }: { cardStage: number }) => {
-  const cardImage = useSelector((state: RootState) => state.card.cardImage);
+  const { cardImage } = useSelector((state: RootState) => state.card);
 
   console.log(cardImage);
 
