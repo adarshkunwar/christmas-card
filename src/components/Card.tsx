49343101ec784fd7116345449410d03cda4c28fc
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-22 00:47:32 +0545

    feat: Add card Note at the back of the card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import { setCardDescription } from "../store/card/cardSlice";
 
 const Card = ({ cardStage }: { cardStage: number }) => {
   const { cardImage, cardName } = useSelector((state: RootState) => state.card);
+  const dispatch = useDispatch();
   const [position, setPosition] = useState({
     x: "50%",
     y: "50%",
@@ -55,7 +57,36 @@ const Card = ({ cardStage }: { cardStage: number }) => {
 
         {/* Back face */}
         <div className="absolute h-full w-full [backface-visibility:hidden] [transform:rotateY(180deg)] rounded-2xl overflow-hidden">
-          <div className="h-full w-full bg-white p-4">backface</div>
+          <div className="h-full w-full bg-white p-8 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgdmlld0JveD0iMCAwIDYwIDYwIj48cGF0aCBkPSJNMzAgMjBMMjAgMzBMMzAgNDBMNDAgMzBMMzAgMjBaIiBmaWxsPSJyZ2JhKDIyMCwgMjAsIDYwLCAwLjEpIi8+PC9zdmc+')] bg-repeat">
+            <div className="relative h-full w-full bg-white/80 rounded-lg p-6 shadow-inner">
+              {/* Decorative Corner Elements */}
+              <div className="absolute top-0 left-0 w-16 h-16 border-t-4 border-l-4 border-red-600/20 rounded-tl-lg"></div>
+              <div className="absolute top-0 right-0 w-16 h-16 border-t-4 border-r-4 border-red-600/20 rounded-tr-lg"></div>
+              <div className="absolute bottom-0 left-0 w-16 h-16 border-b-4 border-l-4 border-red-600/20 rounded-bl-lg"></div>
+              <div className="absolute bottom-0 right-0 w-16 h-16 border-b-4 border-r-4 border-red-600/20 rounded-br-lg"></div>
+
+              {/* Content Area */}
+              <div className="h-full flex flex-col items-center gap-4 pt-8">
+                <div className="text-red-600/80 text-xl font-serif italic">
+                  Dear Friend,
+                </div>
+                <textarea
+                  onChange={(e) => dispatch(setCardDescription(e.target.value))}
+                  placeholder="Write your Christmas message here..."
+                  className="pacifico-regular  flex-1 w-full bg-transparent border-none text-gray-700 placeholder-gray-400/50 focus:outline-none resize-none font-serif text-lg leading-relaxed"
+                  style={{
+                    backgroundImage:
+                      "repeating-linear-gradient(transparent, transparent 39px, #f0f0f0 40px)",
+                    lineHeight: "40px",
+                    paddingTop: "8px",
+                  }}
+                />
+                <div className="text-red-600/80 text-xl font-serif italic self-end pr-8">
+                  With love,
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
