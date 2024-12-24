558f6a8557352b0774e2c9834f797886e7fd917f
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-24 15:45:49 +0545

    feat: play christmas music on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import Background from "./components/Background";
 import Main from "./pages/Main";
 
 function App() {
+  useEffect(() => {
+    const audio = new Audio("/christmas-1.mp3");
+    audio.play();
+  }, []);
   return (
     <div className="relative h-screen w-screen overflow-clip">
       <Main />
